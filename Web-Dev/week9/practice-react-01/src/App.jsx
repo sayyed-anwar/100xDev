16b3eb3ea112e638f75ffea19ec1d5dc2145812a
Anwar-3108 <syed.sahab9628@gmail.com>
2025-03-19 06:41:30 +0530

    Refactor Counter component to use useEffect for automatic increment and comment out buttons, used useEffect for mounting,  logic inside useEffect should run once only on component mount , otherwise it will re-render the whole component when logic run every time and at some point count will increase exponentially

--- Web-Dev/week9/practice-react-01/src/App.jsx
+++ Web-Dev/week9/practice-react-01/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 
@@ -15,12 +15,31 @@ export default App;
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+
+
+  // it will re-render the count state 
+
+// setInterval(()=>{
+//   setCount(count+1)
+// },1000)
+
+console.log("count")
+
+// mount 
+// this will guard setInterval against re-rendering
+useEffect(()=>{
+  setInterval(()=>{
+    setCount((prev)=>prev+1)
+  },1000);
+  console.log("mounted")
+},[])
+
   return (
     <div>
       <h1>Count: {count}</h1>
-      <button onClick={() => setCount((prev)=>prev+1)}>Increment</button>
+      {/* <button onClick={() => setCount((prev)=>prev+1)}>Increment</button>
       <button onClick={() => setCount((prev)=>prev-1)}>Decrease</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setCount(0)}>Reset</button> */}
     </div>
   );
 };
