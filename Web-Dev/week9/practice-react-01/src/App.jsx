bd028b0638691eae1feada83ce2743ab9aa5ed0f
Anwar-3108 <syed.sahab9628@gmail.com>
2025-03-19 07:42:34 +0530

    Implement conditional rendering for Counter component and manage visibility with useEffect for interval toggling

--- Web-Dev/week9/practice-react-01/src/App.jsx
+++ Web-Dev/week9/practice-react-01/src/App.jsx
@@ -3,10 +3,30 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  // conditional rendering
+
+  const [counterVisible, setCounterVisible] = useState(true);
+
+  useEffect(() => {
+    console.log("mounted 02");
+    let clock = setInterval(() => {
+      setCounterVisible((counterVisible) => !counterVisible);
+    }, 5000);
+    // this will never be unmounted 
+    return function () {
+      console.log("unmounted 02");
+      clearInterval(clock);
+    };
+  }, []);
+
   return (
     <>
       <b>hi react</b>
-      <Counter />
+      <button onClick={() => setCounterVisible(!counterVisible)}>
+        Toggle Counter
+      </button>
+      {counterVisible ? <Counter /> : null}
+      {/* {counterVisible && <Counter />} */}
     </>
   );
 }
@@ -16,23 +36,31 @@ export default App;
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  // it will re-render the count state
 
-  // it will re-render the count state 
+  // setInterval(()=>{
+  //   setCount(count+1)
+  // },1000)
 
-// setInterval(()=>{
-//   setCount(count+1)
-// },1000)
+  console.log("count");
 
-console.log("count")
+  // mount
+  // this will guard setInterval against re-rendering
+  useEffect(() => {
+    console.log("mounted 01");
+    let clock = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
 
-// mount 
-// this will guard setInterval against re-rendering
-useEffect(()=>{
-  setInterval(()=>{
-    setCount((prev)=>prev+1)
-  },1000);
-  console.log("mounted")
-},[])
+    // cleanup
+    return function () {
+      console.log("unmounted 01");
+      clearInterval(clock);
+    };
+  }, []);
+
+
+  // basically the code we write in the useEffect is used at the time of mounting and ignore during the re-rendering and the function we return here in useEffect is called at the time of unmounting 
 
   return (
     <div>
