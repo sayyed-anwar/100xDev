d926495c6eeb5bc1bfee0e7efb13fda73b605302
Anwar-3108 <syed.sahab9628@gmail.com>
2025-04-01 23:33:38 +0530

    Refactor WebSocket connection handling to improve message parsing and user management

--- Web-Dev/week16/chat-app/src/index.ts
+++ Web-Dev/week16/chat-app/src/index.ts
@@ -2,32 +2,24 @@ import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 1012 });
 
 interface User {
-    socket : WebSocket,
-    room  : string ;
+  socket: WebSocket;
+  room: string;
 }
 
-
-let allSockets: User [] = [];
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-  allSockets.push(socket);
-  console.log("Client connected");
-  socket.send("client connected");
   socket.on("message", (message) => {
-    const parsedMessages = JSON.parse(message as unknown as string);
-    if (parsedMessages.type === "join") {
-        allSockets.push({
-            socket,
-            room: parsedMessages.payload.room ;
-        });
+    //@ts-ignore
+    const parsedMessages = JSON.parse(message);
+    if(parsedMessages.type === "join"){
+      allSockets.push({
+        socket,
+        room: parsedMessages.room.roomId,
+      });
     }
-    allSockets.forEach((sock) => {
-      sock.send(message.toLocaleString());
-    });
-    console.log("received message: " + message.toString());
-  });
-  socket.on("disconnect", () => {
-    allSockets = allSockets.filter((sock) => sock !== socket);
-    console.log("Client disconnected");
+    if(parsedMessages.type === "chat"){
+      
+    }
   });
 });
